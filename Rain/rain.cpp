87d1faa30d11c48feb35e05fd66e8ede0d6ae4e4
kaucrow <kaucrow@protonmail.com>
2023-08-05 13:15:10 -0400

    The rain cycles, title gets printed, one col only

--- Rain/rain.cpp
+++ Rain/rain.cpp
@@ -9,57 +9,60 @@ void ClrScr();
 int main(){
     srand(time(NULL));
     char rain[7] = {'@','%','#','+','=','*','.'};
-    char title[7] = {'h','e','l','l','o','w','r'};
-    int rainLimit = 6, rainPos = -1, rainRow = -1, dice, rainCount = 0, counter = 1, titleLimit = 0;
-    bool rainType, titleFlag = 0;
+    char title[9] = {'h','e','l','l','o','w','r',' ',' '};
+    int rainLimit = 6, titleLimit = 0;          // limit of chars to print of rain and title arrays
+    int rainPos = 0, titlePos = 0;              // current char being printed from rain or title array
+    int rainRow = -1;                           // row where the next rain char will be printed on
+    int rainCount = 0;                          // amount of rain streaks fallen
+    bool titleFlag = 0;                         // flag for indicating that the title is being printed
     
     while(true){
         for(int row = 0; row < 20; row++){
             
-            // when rain is inactive 
+            // WHEN RAIN IS INACTIVE
             if(rainRow == -1){
+                // if the title hasn't been printed yet, try to generate a new rain streak
                 if(rainCount <= 3){
-                    dice = 0;               // roll dice
-                    if(dice == 0){
-                        rainRow = 0;
-                        rainType = 1;//rand()%2;
-                    }
+                    if(rand()%1 == 0){ rainRow = 0; }
                 }
+                // if the title has just been printed, keep printing it on the same position
                 else if(row >= 10 && row <= 16){ cout << title[row - 10]; } 
             }
-            // when title is active
+
+            // WHEN TITLE IS ACTIVE
             if(titleFlag == 1){ 
-                if(row <= rainRow - titleLimit - 1 && rainRow >= 10){
-                    cout << title[titleLimit];
-                    titleLimit++;
+                if(row >= rainRow - titleLimit && row < rainRow){
+                    cout << title[titlePos];
+                    titlePos++;
                 }
-               // else if(row == )
             }
-            // when rain is active
+
+            // WHEN RAIN IS ACTIVE 
             if(rainRow == row){
-                rainPos++;
-                //if(rainCount == 3 && row == 10){ cout << "HERE"; rainRow++; }                  
+                // if there are still rain chars left to print
                 if(rainPos <= rainLimit){
                     cout << rain[rainPos];
                     rainRow++;
+                    rainPos++;
                 }
+                // if all rain chars have been printed
                 else{
-                    /*if(rainType == 0){
-                        if(rainRow >= 13){ rainLimit--; }//cout << rainLimit; }
-                        rainRow -= rainLimit;
-                    }*/
-                    if(rainType == 1){
-                        rainRow -= rainLimit;
-                        if(rainRow >= 13){ rainLimit--; }//cout << rainLimit;}
-                    }
+                    // if rain is normal, update the starting rainRow normally
+                    rainRow -= rainLimit;
+                    // if the rain is ending, reduce the amount of rain chars to print
+                    if(rainRow >= 13){ rainLimit--; }
+                    // if the rainCount is 3 and the starting rainRow is in the range of the title,
+                    // increase the amount of title chars to print
+                    if(rainCount == 3 && rainRow >= 11){ titleLimit++; }
+                    // if the amount of rain chars is -1, the rain has ended for the curr column
                     if(rainLimit < 0){ 
                         rainRow = -1; rainLimit = 6; rainCount++; 
-                        if(rainCount == 3){ cout << "Hello"; titleFlag = 1;}
+                        if(rainCount == 3){ titleFlag = 1;}
                     }
-                    rainPos = -1; titleLimit = 0;
+                    // reset the rainPos and titlePos
+                    rainPos = 0; titlePos = 0;
                 }
             }
-            //cout << "rainPos: " <<rainPos;
             cout << '\n';
         }
         getchar();
