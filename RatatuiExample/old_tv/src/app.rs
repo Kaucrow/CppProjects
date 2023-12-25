29d3bd66ca401c5150bb7383d3e9e7ce6f5635fa
kaucrow <121270913+Kaucrow@users.noreply.github.com>
2023-12-25 12:49:55 -0400

    feat: Added `old_tv` ratatui example. Dither mask now gets updated
    TODO: Build the ui

---
+++ RatatuiExample/old_tv/src/app.rs
@@ -0,0 +1,33 @@
+use std::collections::BTreeMap;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Dither {
+    Normal,
+    Light,
+}
+
+#[derive(Debug, PartialEq, PartialOrd)]
+pub enum Status {
+    Index(IndexType),
+    Ready,
+    Done,
+}
+
+pub type IndexType = u8;
+
+pub struct Ferris {
+    pub normal: Vec<String>,
+    pub light: Vec<String>,
+    //pub to_draw: BTreeMap<Index, Dither>,
+    pub mask: Vec<(Dither, Status)>
+}
+
+impl Ferris {
+    pub fn new<const U: usize>(mask: [Dither; U]) -> Self {
+        Self {
+            normal: Vec::new(),
+            light: Vec::new(),
+            mask: mask.into_iter().map(|dither| (dither, Status::Ready)).collect(),
+        }
+    }
+}
---
+++ RatatuiExample/old_tv/src/main.rs
@@ -0,0 +1,152 @@
+use ratatui::{
+    backend::{
+        Backend,
+        CrosstermBackend,
+    },
+    Terminal,
+};
+
+use crossterm::{
+    event::{
+        self,
+        EnableMouseCapture,
+        DisableMouseCapture,
+        Event,
+        KeyEventKind,
+        KeyCode,
+    },
+    execute,
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+};
+
+use std::{time::{Instant, Duration}, collections::BTreeMap};
+
+use std::io::{self, BufRead, BufReader};
+use std::fs::File;
+
+use anyhow::Result;
+
+mod app;
+
+use app::{ Ferris, Dither, IndexType, Status };
+
+fn main() -> Result<()>{
+    let data_path = {
+        let mut path = String::from(std::env::current_exe().unwrap().to_string_lossy());
+        path = path[..=path.find("old_tv").expect("could not find `old_tv` folder") + ("old_tv").len()].to_string();
+        if cfg!(windows){
+            path.push_str("src\\data\\");
+        } else {
+            path.push_str("src/data/");
+        }
+        path
+    };
+
+    let mut ferris = Ferris::new([Dither::Light, Dither::Light, Dither::Normal, Dither::Normal, Dither::Light, Dither::Light]);
+
+    fn get_ferris_lines<'a>(filepath: &str, ferris: & mut Ferris, light_variant: bool) -> Result<()> {
+        let file = File::open(filepath)?;
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            let line_unwrapped = line?;
+            if light_variant {
+                ferris.light.push(line_unwrapped.clone());
+            } else {
+                //ferris.to_draw.insert(ferris.normal.len() as Index, Dither::Normal);
+                ferris.normal.push(line_unwrapped.clone());
+            }
+            println!("{}", &line_unwrapped);
+        }
+
+        Ok(())
+    }
+    get_ferris_lines(&(data_path.clone() + "ferris.txt"), &mut ferris, false).expect("Error reading and writing");
+    get_ferris_lines(&(data_path.clone() + "ferris_light.txt"), &mut ferris, true).expect("Error reading and writing");
+
+    if ferris.normal.len() != ferris.light.len() { return Err(anyhow::anyhow!("Normal and light variants must have the same number of lines")) };
+
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let res = run_app(&mut terminal, &mut ferris);
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture,
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{err:?}");
+    }
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    ferris: &mut Ferris,
+
+) -> Result<()> {
+    let mut last_update = Instant::now();
+    let update_rate = Duration::from_millis(250);
+    let mut should_update = true;
+
+    loop {
+        if Instant::now() - last_update >= update_rate {
+            should_update = true;
+            last_update = Instant::now();
+        }
+
+        if should_update {
+            for (_, index) in ferris.mask.iter_mut() {
+                match *index {
+                    Status::Ready => {
+                        *index = Status::Index((ferris.normal.len() - 1) as IndexType);
+                        break;
+                    },
+                    Status::Index(ref mut value) => {
+                        if *value > 0 {
+                            *value -= 1;
+                        } else {
+                            *index = Status::Done;
+                        }
+                    },
+                    _ => {}
+                }
+            }
+
+            should_update = false;
+            println!("{:?}", ferris.mask);
+        }
+
+
+        if ferris.mask.iter().all(|(_, index)| index == &Status::Done) {
+            ferris.mask.iter_mut().for_each(|(_, index)| *index = Status::Ready);
+        }
+
+        /*if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                continue;
+            }
+            match key.code {
+                KeyCode::Char('q') => {
+                    return Ok(());
+                }
+                _ => {}
+            }
+        }*/
+        //terminal.draw(|f| ui(f, ferris))?;
+    }
+    Ok(())
+}
